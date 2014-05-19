c79a9c2d3eedc0cc49dd0197b99b297b7fe65077
Ole Aamot <oka@oka.no>
2014-05-19 17:02:13 +0200

    SQL file for location data structure

---
+++ location.sql
@@ -0,0 +1,11 @@
+CREATE TABLE location (
+       id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+       name TEXT NOT NULL,
+       KEY name_idx (name(2048)),
+       glat DECIMAL(10, 8) NOT NULL,
+       glon DECIMAL(11, 8) NOT NULL,
+       ggeo GEOMETRY NOT NULL,
+       SPATIAL INDEX(ggeo)
+) Engine="MyISAM";
+
+CREATE SPATIAL INDEX location_geo ON location (ggeo);
