57afa00e86f0a8ffe74448b15aae973de96da1d0
Ole Aamot <oka@oka.no>
2014-06-05 21:34:27 +0200

    Location

--- android/src/gl/location/location/MainActivity.java
+++ android/src/gl/location/location/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends Activity
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         Intent sendIntent = new Intent (Intent.ACTION_VIEW);
-        Uri uri = Uri.parse("http:/location.gl/Global");
+        Uri uri = Uri.parse("http:/location.gl/Location");
 		sendIntent.setData(uri);
 		startActivity(sendIntent);
     }
