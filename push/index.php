ef5e19570a6f876fb2bc8e6d18ff8bdc4f973b1f
Ole Aamot <oka@oka.no>
2014-06-21 15:24:28 -0400

    Update Location push/pull

--- push/index.php
+++ push/index.php
@@ -1,13 +1,13 @@
 <?php
 
-$uploaddir = 'location/';
+$uploaddir = 'location/push/';
 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']) . time();
 
 echo '<pre>';
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "File is valid, and was successfully uploaded.\n";
+    echo "Push was valid and successful.\n";
 } else {
-    echo "Duplicate upload.  RETRY in 15 minutes.\n";
+    echo "Duplicate push.  RETRY in 15 minutes.\n";
 }
 
 echo 'Here is some more debugging info:';
